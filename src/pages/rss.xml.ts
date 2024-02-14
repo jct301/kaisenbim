2b19af367508cc5fff5a0414aca5cd50d23b76a0
jctr301 <jctr301@gmail.com>
2024-02-14 19:59:13 -0400

    fixed rss and update dependencies

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,22 @@
-import rss from "@astrojs/rss";
-import { SITE_CONFIG } from "../constants";
-import { getAllProjects } from "../utils/project";
+import { getContent } from '@/services/content'
+import rss from '@astrojs/rss'
+import { SITE_CONFIG } from '../constants'
 
 export const GET = async () => {
-  const projects = await getAllProjects();
+  const projects = await getContent({ type: 'project' })
 
   return rss({
     title: SITE_CONFIG.title,
     description: SITE_CONFIG.description,
     site: import.meta.env.SITE,
-    items: projects.map((project) => ({
-      title: project.data.title,
-      description: project.data.description,
-      coverImage: project.data.coverImage,
-      link: `projects/${project.slug}`,
-    })),
-  });
-};
+    items: projects.map(
+      ({ data: { description, title, thumbnail, publishDate }, slug }) => ({
+        title,
+        description,
+        coverImage: thumbnail,
+        pubDate: publishDate,
+        link: `projects/${slug}`,
+      })
+    ),
+  })
+}
