59c86ed1d99f26224ea0af51a8d5a58959facf10
jctr301 <jctr301@gmail.com>
2024-02-27 13:08:28 -0400

    change image size for default

--- src/components/copy-to-clipboard.svelte
+++ src/components/copy-to-clipboard.svelte
@@ -15,7 +15,7 @@
   <div class="relative">
     <input
       type="text"
-      class="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      class="col-span-6 bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-100 dark:focus:ring-blue-500 dark:focus:border-blue-500"
       bind:value={textToCopy}
       disabled
       readonly
